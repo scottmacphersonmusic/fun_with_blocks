cd124d79cf9c7b4dc190852a10205befe5c37379
scott <scottmacphersonmusic@gmail.com>
2015-07-22 19:07:09 -0700

    Test assert_equal rather than stout

--- test/map_test.rb
+++ test/map_test.rb
@@ -4,6 +4,6 @@ require './new_map'
 class MapTest < MiniTest::Test
   def test_my_map
     a = ["one", "two", "three"]
-    assert_output(["ONE", "TWO", "THREE"]) { a.my_map { |element| element.upcase }}
+    assert_equal ["ONE", "TWO", "THREE"], (a.my_map { |element| element.upcase })
   end
 end
